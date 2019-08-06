1f289689472195314d0b0a5c22d169ae80b1f01d
IsaacSullivan <eyezaque@gmail.com>
2019-08-06 18:10:35 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Link from "gatsby-link";
 
 export default () =>
- <div style={{ color: `tomato` }}>
-   <h1>Hello Gatsby!</h1>
+ <div style={{ color: `black` }}>
+   <h1>Isaac Sullivan</h1>
    <p>What a world.</p>
    <img src="https://source.unsplash.com/random/400x200" alt="" />
    <br />
