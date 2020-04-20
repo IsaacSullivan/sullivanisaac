170245d4059de7ba275b1bd3dd94fe5054e347bc
IsaacSullivan <eyezaque@gmail.com>
2020-04-19 21:34:27 -0500

    form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import Link from "gatsby-link";
 
-export default () =>
- <div style={{ color: `black` }}>
-   <h1>Isaac Sullivan</h1>
-   <p>What a world.</p>
-   <img src="https://source.unsplash.com/random/400x200" alt="" />
-   <br />
-    <div>
-      <Link to="/page-2/">Link</Link>
-    </div>
-    <div>
-      <Link to="/counter/">Counter</Link>
-    </div>
- </div>
+export default class IndexPage extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    alert(`Welcome ${this.state.firstName} ${this.state.lastName}!`)
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          First name
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label>
+          Last name
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
